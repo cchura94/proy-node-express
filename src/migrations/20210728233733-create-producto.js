56538eef2437a59cf8ccf03099ed4414b25c207a
cchura94 <cchura.cpc@gmail.com>
2021-07-28 20:03:27 -0400

    Models y migration

---
+++ src/migrations/20210728233733-create-producto.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Productos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      precio: {
+        type: Sequelize.DECIMAL
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      descripcion: {
+        type: Sequelize.TEXT
+      },
+      imagen: {
+        type: Sequelize.STRING
+      },
+      categoriaId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Categoria",
+          key: "id"
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Productos');
+  }
+};
---
+++ src/migrations/20210728233825-create-cliente.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Clientes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombres: {
+        type: Sequelize.STRING
+      },
+      apellidos: {
+        type: Sequelize.STRING
+      },
+      ci_nit: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Clientes');
+  }
+};
